a674e940e02591ae86ffe6510b48e5a1ed65b25e
yupi-pi <lensyonen@gmail.com>
2023-02-15 19:43:25 +0900

    コメントしてあるプロトタイプが削除できるように実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
     validates :position
   end
 
-  has_many :prototypes
-  has_many :comments
+  has_many :prototypes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
